5fd8a3ea7c18e7cdc4f591e9302f69e720210bde
Marcelo <marcelocdias93@gmail.com>
2019-10-10 21:44:14 -0300

    Adding comments and an error handler

--- lexical_analyzer.rb
+++ lexical_analyzer.rb
@@ -1,9 +1,32 @@
+# -------------------------------- *
+# Universidade Federal de Goiás    *
+# Instituto de Informática         *
+# Creation date:   10/08/19        *
+# Last updated on: 10/08/19        *
+# Author: Marcelo Cardoso Dias     *
+# -------------------------------- */
+
+# cafezinho_lexical_analyzer.rb
+
+# --------------------------------------------------- *
+# This is a lexical analyzer for Cafezinho language,  *
+# a programming language invented for a study purpose *
+# in Compilers class.                                 *
+# --------------------------------------------------- */
+
+
+# REQUIREMENTS ----------------------------------------
 require "rly"
+
+# -----------------------------------------------------
+
 
 class CafezinhoLex < Rly::Lex
 	literals '+-*/?!,:;[](){}%=><'
 
 	ignore " \t\n"
+
+	#ignore /\/\*.*\*\//
 
 	token :NUMBER, /\d+/ do |t|
 		t.value = t.value.to_i
@@ -12,11 +35,18 @@ class CafezinhoLex < Rly::Lex
 
 	token :ID, /[a-zA-Z]+[0-9a-zA-Z]*/
 	
-	token :RESERVED_WORD, /programa|car|int|retorne|leia|escreva|novalinha|se|entao|senao|enquanto|execute/
+	token :RESERVED_WORD, /programa|int|car|intconst|carconst|retorne|leia|escreva|novalinha|se|entao|senao|enquanto|execute/
+
+	on_error do |t|
+	    puts "Illegal character: #{t.value}"
+
+	    t.lexer.pos += 1
+
+	    nil
+	end
 end
 
-str = '2+2nesnte
-asdf	dei tab'
+str = '2+2$'
 
 lex = CafezinhoLex.new(str)
 
