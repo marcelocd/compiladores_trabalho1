ff1f0f3900c15b4a5c1d175493a534fce3bc3b89
Marcelo <marcelocdias93@gmail.com>
2019-10-13 02:46:47 -0300

    Adding context free grammar unfinished rules

--- cafezinho_compiler.rb
+++ cafezinho_compiler.rb
@@ -32,9 +32,9 @@ class CafezinhoLex < Rly::Lex
 	# -----------------------------------------
 
 	# TOKENS ----------------------------------
-	token :COMMENT, /\/\*[^\*]*\*+([^[\*\/]][^\*]*\*+)*\// do end
+	token :comment, /\/\*[^\*]*\*+([^[\*\/]][^\*]*\*+)*\// do end
 	
-	token :UNFINISHED_COMMENT, /\/\*.*/ do |t|
+	token :unfinished_comment, /\/\*.*/ do |t|
 		if t.type.to_s == 'UNFINISHED_COMMENT'
 			puts 'ERRO: COMENTARIO NAO TERMINA'
 		end
@@ -42,21 +42,23 @@ class CafezinhoLex < Rly::Lex
 		nil
 	end
 
-	token :NUMBER, /\d+/ do |t|
+	token :intconst, /\d+/ do |t|
 		t.value = t.value.to_i
 		t
 	end
 
-	token :CONST_STRING, /\"[^\"]+\"/
+	token :carconst, /[a-zA-Z]/
 
-	token :RESERVED_WORD, /programa|int|car|intconst|carconst|retorne|leia|escreva|novalinha|se|entao|senao|enquanto|execute|e|ou/
+	token :const_string, /\"[^\"]+\"/
 
-	token :ID, /[a-zA-Z]+[0-9a-zA-Z]*/
+	token :reserved_word, /programa|int|car|intconst|carconst|retorne|leia|escreva|novalinha|se|entao|senao|enquanto|execute|e|ou/
+
+	token :id, /[a-zA-Z]+[0-9a-zA-Z]*/
 
 	# -----------------------------------------
 
 	on_error do |t|
-	   puts "ERRO: CARACTER INVALIDO"
+	   puts "ERRO: CARACTER INVALidO"
 
 	   t.lexer.pos += 1
 
@@ -68,6 +70,105 @@ end
 
 # SYNTACTICAL ANALYZER -----------------------
 class CafezinhoParse < Rly::Yacc
+	rule 'DeclFuncVar DeclProg'
+
+	rule 'DeclFuncVar : Tipo id DeclVar ; DeclFuncVar
+							| Tipo id [intconst] DeclVar ; DeclFuncVar
+							| Tipo id DeclFunc DeclFuncVar
+							| '
+
+	rule 'DesclProg : programa Bloco'
+	
+	rule 'DeclVar : ,id DeclVar
+					  | ,id[intconst]DeclVar
+					  | '
+	
+	rule 'DeclFunc : ( ListaParametros )Bloco'
+	
+	rule 'ListaParametros : 
+								 | ListaParametrosCont'
+
+	rule 'ListaParametrosCont : Tipo id
+									  | Tipo id[]
+									  | Tipo id, ListaParametrosCont
+									  | Tipo id[]. ListaParametrosCont'
+	
+	rule 'Bloco : { ListaDeclVar ListaComando }
+					| { ListaDeclVar }'
+	
+	rule 'ListaDeclVar : 
+							 | Tipo id DeclVar ; ListaDeclVar
+							 | Tipo id[intconst]DeclVar ; ListaDeclVar'
+	
+	rule 'Tipo : int
+				  | car'
+	
+	rule 'ListaComando : Comando
+							 | Comando ListaComando'
+
+	rule 'Comando : ;
+					  | Expr ;
+					  | retorne Expr ;
+					  | leia LValueExpr :
+					  | escreva Expr ;
+					  | escreva "cadeiaCaracteres" ;
+					  | novalinha ;
+					  | se(Expr)entao Comando
+					  | se(Expr)entao Comando senao Comando
+					  | enquanto(Expr)execute Comando
+					  | Bloco'
+
+	rule 'Expr : AssignExpr'
+
+	rule 'AssignExpr : CondExpr
+						  | LValueExpr = AssignExpr'
+
+	rule 'CondExpr : OrExpr
+						| OrExpr ? Expr : CondExpr'
+
+	rule 'OrExpr : OrExpr ou AndExpr
+					 | AndExpr'
+
+	rule 'AndExpr : AndExpr e EqExpr
+					  | EqExpr'
+
+	rule 'EqExpr : EqExpr == DesigExpr
+					 | EqExpr != DesigExpr
+					 | DesigExpr'
+
+	rule 'DesigExpr : DesigExpr < AddExpr
+						 | DesigExpr > AddExpr
+						 | DesigExpr >= AddExpr
+						 | DesigExpr <= AddExpr
+						 | AddExpr'
+
+	rule 'AddExpr : AddExpr + MultExpr
+					  | AddExpr - MulExpr
+					  | MulExpr'
+
+	rule 'MulExpr: MulExpr * UnExpr
+					 | MulExpr / UnExpr
+					 | MulExpr % UnExpr
+					 | UnExpr'
+
+	rule 'UnExpr : - PrimExpr
+					 | ! PrimExpr
+					 | PrimExpr'
+
+	rule 'LValueExpr : id [Expr]
+						  | id'
+
+	rule 'PrimExpr : id (ListExpr)
+						| id ()
+						| id [Expr]
+						| id
+						| carconst
+						| intconst'
+
+	rule 'ListExpr : AssignExpr
+						| ListExpr , AssignExpr'
+
+	# EXAMPLES --------------------------------
 	rule 'statement : expression' do |st, e|
 		st.value = e.value
 	end
@@ -82,6 +183,8 @@ class CafezinhoParse < Rly::Yacc
 	rule 'expression : NUMBER' do |ex, n|
 		ex.value = n.value
 	end
+
+	# -----------------------------------------
 end
 
 # --------------------------------------------
